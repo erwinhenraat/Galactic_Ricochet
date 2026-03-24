852be6e3b7ba6ddb887c88d176899812f9addc14
calvinNL11 <calvinkop.2005@gmail.com>
2026-03-24 11:21:34 +0100

    ball groter

--- Assets/Scripts/random combo reward/SuperBallMarker.cs
+++ Assets/Scripts/random combo reward/SuperBallMarker.cs
@@ -4,15 +4,30 @@ public class SuperBallMarker : MonoBehaviour
 {
     public bool IsSuperBall { get; private set; }
 
+    private Vector3 originalScale;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
     public void Enable()
     {
         IsSuperBall = true;
+
+        // Make ball bigger
+        transform.localScale = originalScale * 1.5f;
+
         Debug.Log("[SuperBallMarker] Ball is now SUPER BALL");
     }
 
     public void Disable()
     {
         IsSuperBall = false;
+
+        // Restore original size
+        transform.localScale = originalScale;
+
         Debug.Log("[SuperBallMarker] Ball returned to NORMAL");
     }
 }
