57ec0d05023a42fc5abf0794519e52f4922d64ac
pocoloco008 <pacovanderaar@gmail.com>
2026-03-25 12:40:26 +0100

    fw

--- Assets/Scripts/Audio/PlaySounds.cs
+++ Assets/Scripts/Audio/PlaySounds.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.Splines;
-
 
 public class PlaySounds : MonoBehaviour
 {
-    public enum SoundType { Bumper, Combo, GameOver, ExtraBall, BallLost, Loadup, RailEnter, RailRoll, FlipperHit, RNGBumper }
+    public enum SoundType { Bumper, Combo, GameOver, ExtraBall, BallLost, Loadup, RNGBumper }
 
     private Dictionary<SoundType, AudioSource> soundSources = new Dictionary<SoundType, AudioSource>();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
+        // Initialize all audio sources from components
         AudioSource[] sources = GetComponents<AudioSource>();
 
-        if (sources.Length < 10)
+        if (sources.Length < 7)
         {
-            Debug.LogError("PlaySounds requires 10 AudioSource components!");
+            Debug.LogError("PlaySounds requires 7 AudioSource components!");
             return;
         }
 
@@ -25,10 +24,7 @@ public class PlaySounds : MonoBehaviour
         soundSources[SoundType.ExtraBall] = sources[3];
         soundSources[SoundType.BallLost] = sources[4];
         soundSources[SoundType.Loadup] = sources[5];
-        soundSources[SoundType.RailEnter] = sources[6];
-        soundSources[SoundType.RailRoll] = sources[7];
-        soundSources[SoundType.FlipperHit] = sources[8];
-        soundSources[SoundType.RNGBumper] = sources[9];
+        soundSources[SoundType.RNGBumper] = sources[6];
 
         HitBumper.onHitBumper += PlayBumper;
         Combo.onComboAchieved += PlayCombo;
@@ -37,14 +33,9 @@ public class PlaySounds : MonoBehaviour
         PlayArea.onBallLost += PlayBallLost;
         Shoot.onPress += PlayLoadup;
         Shoot.onRelease += StopLoadup;
-        BallToRailConnector.onRailPlaySound += PlayRailEnter;
-        BallToRailConnector.onRailPlaySound += PlayRailRoll;
-        BallToRailConnector.onRailStopSound += StopRailRoll;
-        FlipperController.onFlipperPlaySound += PlayFlipperHit;
         RNGHitBumper.onHitRNGBumper += PlayRNGBumper;
-        sources = GetComponents<AudioSource>();
-
     }
+
     private void OnDisable()
     {
         HitBumper.onHitBumper -= PlayBumper;
@@ -54,32 +45,33 @@ public class PlaySounds : MonoBehaviour
         PlayArea.onBallLost -= PlayBallLost;
         Shoot.onPress -= PlayLoadup;
         Shoot.onRelease -= StopLoadup;
-        BallToRailConnector.onRailPlaySound -= PlayRailEnter;
-        BallToRailConnector.onRailPlaySound -= PlayRailRoll;
-        BallToRailConnector.onRailStopSound -= StopRailRoll;
-        FlipperController.onFlipperPlaySound -= PlayFlipperHit;
         RNGHitBumper.onHitRNGBumper -= PlayRNGBumper;
     }
+
     private void PlayRNGBumper(Transform _, int __)
     {
         soundSources[SoundType.RNGBumper].pitch = Random.Range(0.5f, 1.5f);
         soundSources[SoundType.RNGBumper].Play();
     }
+
+
     private void PlayBumper(Transform _, int __)
     {
         soundSources[SoundType.Bumper].pitch = Random.Range(0.5f, 1.5f);
         soundSources[SoundType.Bumper].Play();
-
     }
+
     private void PlayCombo(int value, string _)
     {
         soundSources[SoundType.Combo].pitch = 1 + value / 10;
         soundSources[SoundType.Combo].Play();
     }
+
     private void PlayGameOver(string _)
     {
         soundSources[SoundType.GameOver].Play();
     }
+
     private void PlayExtraBall(string _)
     {
         soundSources[SoundType.ExtraBall].Play();
@@ -98,21 +90,5 @@ public class PlaySounds : MonoBehaviour
     private void StopLoadup()
     {
         soundSources[SoundType.Loadup].Stop();
-    }
-    private void PlayRailEnter(bool bl)
-    {
-        soundSources[SoundType.RailEnter].Play();
-    }
-    private void PlayRailRoll(bool bl)
-    {
-        soundSources[SoundType.RailRoll].Play();
-    }
-    private void StopRailRoll(bool bl)
-    {
-        soundSources[SoundType.RailRoll].Stop();
-    }
-    private void PlayFlipperHit(bool bl)
-    {
-        soundSources[SoundType.FlipperHit].Play();
     }
 }
